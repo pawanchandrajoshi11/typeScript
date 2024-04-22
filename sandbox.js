08a67e2587aaad72c0c672e16eb9ea40d66beb27
Pawan Chandra Joshi <pawanpcj600@gmail.com>
2024-04-22 16:20:30 +0530

    .

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+// character = 20;
+character = 'luigi';
+var circle = function (diameter) {
+    return diameter * 10;
+};
+circle(3);
--- sandbox.ts
+++ sandbox.ts
@@ -1,2 +1,13 @@
-const character = 'mario';
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
 
+// character = 20;
+character = 'luigi';
+
+const circle = (diameter: number) => {
+    return diameter * 10;
+}
+
+
+circle(3)
